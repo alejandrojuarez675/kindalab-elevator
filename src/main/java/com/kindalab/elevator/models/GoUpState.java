2f632bb4c44ed2e93ad54a6196fea54ccac26556
Alejandro Juarez <alejandrojuarez675@gmail.com>
2022-03-27 17:54:40 -0300

    commit for rest to enjoy sunday afternoon

--- src/main/java/com/kindalab/elevator/models/GoUpState.java
+++ src/main/java/com/kindalab/elevator/models/GoUpState.java
@@ -1,8 +1,5 @@
 package com.kindalab.elevator.models;
 
-import java.util.Comparator;
-import java.util.Optional;
-
 public class GoUpState implements ElevatorState {
 
     private final Elevator elevator;
@@ -28,25 +25,6 @@ public class GoUpState implements ElevatorState {
     @Override
     public void goToFloor(Long floor) {
         this.elevator.addRequiredFloor(floor);
-        this.goingUp();
-    }
-
-    private void goingUp() {
-        if (!this.elevator.getRequiredList().isEmpty()) {
-            Optional<Long> minRequiredFloorOpt = this.elevator.getRequiredList().stream()
-                    .filter(x -> x >= this.elevator.getCurrentFloor())
-                    .min(Comparator.naturalOrder());
-
-            if (minRequiredFloorOpt.isPresent()) {
-                Long minRequiredFloor = minRequiredFloorOpt.get();
-
-                while (minRequiredFloor > this.elevator.getCurrentFloor()) {
-                    System.out.println("Up " + this.elevator.getCurrentFloor());
-                    this.elevator.setCurrentFloor(this.elevator.getCurrentFloor() + 1);
-                }
-            }
-        }
-        this.elevator.setState(this.elevator.getStoppedState());
     }
 
 }
